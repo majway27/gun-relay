12db6665e3154a2470803ec51883f7be0057bdb7
majway27 <robert.p.may@gmail.com>
2022-05-12 05:06:33 -0600

    basic relay, start cmd

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gun-relay
+
+## Source
+
 https://github.com/amark/gun/blob/master/examples/http.js
+
+## Running
+
+`npm start`
---
+++ server.js
@@ -0,0 +1,4 @@
+const GUN = require('gun/gun');
+
+const server = require('http').createServer().listen(8080);
+const gun = GUN({web: server});
