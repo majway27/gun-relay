2a8edceaed09c9d0625f5fc39f016f7240365a50
majway27 <robert.p.may@gmail.com>
2022-05-15 15:57:27 -0600

    relay log, test

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const GUN = require('gun/gun');
+var http = require('http');
 
-const server = require('http').createServer().listen(8080);
+//counter = 1
+console.log('relay start')
+//console.log('start counter: ' +  counter)
+server = http.createServer(function (req, res) {
+    //console.log('counter: ' +  counter)
+    //counter = counter + 1
+    res.write('Hello World!'); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
+//const server = require('http').createServer().listen(8080);
 const gun = GUN({web: server});
